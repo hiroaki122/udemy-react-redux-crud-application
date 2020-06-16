7826fe8606857d5762c83d5f4365cdbe0b76682a
hiroaki122 <jiro.h.v.122@gmail.com>
2020-06-16 11:48:51 +0900

    learn props

--- src/App.js
+++ src/App.js
@@ -3,12 +3,23 @@ import { render } from '@testing-library/react';
 
 
 function App() {
+    const profiles = [
+      {name: "Taro", age: 10},
+      {name: "Hanako", age: 5}
+    ]
     return (
       <div>
-        <label htmlFor="bar">bar</label>
-        <input type="text" onChange={() => {console.log("I am clicked!")}} />
+        {
+          profiles.map((profile,index) => {
+            return <User name ={profile.name} age={profile.age} key={index} />
+          })
+        }
       </div>
     )
+}
+
+const User =(props) => {
+return <div>Hi, I am {props.name}, and {props.age}years old.</div>
 }
 
 export default App;
